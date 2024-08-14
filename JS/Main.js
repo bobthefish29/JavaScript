e56e7748e2e0b4cd81ade044443af8ee060221fe
bobthefish29 <dapunchak@email.neit.edu>
2024-08-14 00:54:12 -0400

    HadALittleToMuchFunOnIt

--- JS/Main.js
+++ JS/Main.js
@@ -1,21 +1,26 @@
 
 
-let section = q(`section`, ``)
 
-console.log(section)
 
-function q(element, number){
+
+
+
+// let input = q(`input`, 2)
+
+// console.log(section)
+
+// function q(element, number){
 
     
 
-    let temp;
-    //this is checking if its an a or how many items the user wants to select
-    if(number == `a`){
-        temp = document.querySelectorAll(element)
-    }
-    else{
-        temp.document.querySelector(element)
-    }
+//     let temp;
+//     //this is checking if its an a or how many items the user wants to select
+//     if(number == `a`){
+//         temp = document.querySelectorAll(element)
+//     }
+//     else{
+//         temp.document.querySelector(element)
+//     }
 
-    return temp;
-}
+//     return temp;
+// }
