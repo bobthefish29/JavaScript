22f569a02f0ceff26cf5dc7b2dad03c8b5b91e69
bobthefish29 <dapunchak@email.neit.edu>
2024-08-13 23:09:45 -0400

    PlayingAroundWithHTML

---
+++ JS/Main.js
@@ -0,0 +1,21 @@
+
+
+let section = q(`section`, ``)
+
+console.log(section)
+
+function q(element, number){
+
+    
+
+    let temp;
+    //this is checking if its an a or how many items the user wants to select
+    if(number == `a`){
+        temp = document.querySelectorAll(element)
+    }
+    else{
+        temp.document.querySelector(element)
+    }
+
+    return temp;
+}
